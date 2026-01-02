6481005d191e02e6eb7be3374760d92a07b65627
snowflakeam <a.mera@nhs.net>
2026-01-02 11:29:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -54,6 +55,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' ' #+= operator means "add this to what is already in the variable"
+        #added from line 80
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        
         
     #st.write(ingredients_string)
 
@@ -73,10 +78,10 @@ if ingredients_list:
 
 
 #New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+ #import requests
+ #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+ #sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
 
